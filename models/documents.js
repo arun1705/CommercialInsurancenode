9011353cf65f925acb63fac7300732cd13d96974
arun1705 <arunhosamani93@gmail.com>
2017-08-14 16:11:50 +0530

    modified

--- models/documents.js
+++ models/documents.js
@@ -1,27 +1,22 @@
 'use strict';
 
+
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const docSchema = mongoose.Schema({
-    path: {
+
+var Photo = mongoose.Schema({
+    url: {
         type: String,
-        required: true,
-        trim: true
+        length: 255
     },
-    originalname: {
-        type: String,
-        required: true
-    },
-    userid: String,
+
+    userid: String
 
 });
-
-
-
-
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://rpqbci:rpqb123@ds163721.mlab.com:63721/commercialinsurance', { useMongoClient: true });
-
-module.exports = mongoose.model('files', docSchema);
+mongoose.connect('mongodb://rpqbci:rpqb123@ds163721.mlab.com:63721/commercialinsurance', {
+    useMongoClient: true
+});
+module.exports = mongoose.model('files', Photo);
